9b1989b2f876919c1c4746748e842fb2ee9f3baa
QuinnWootton <164350294+QuinnWootton@users.noreply.github.com>
2024-04-22 13:03:40 +0000

    here

--- static/script.js
+++ static/script.js
@@ -1,1 +1,7 @@
+function getDate(){
+	const d = new Date();
+    document.getElementById("date").innerHTML = d;		
+    // TBD display current date in the span tag with id="date" in web page footer
+    // hint: https://www.w3schools.com/js/tryit.asp?filename=tryjs_date_current
+}
 
